4caefef274d484a90bb8f6855756c25878ccdb63
Forest Young <111620017+Grocherio@users.noreply.github.com>
2024-05-01 12:31:31 -0600

    Add files via upload

---
+++ src/Foo.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <string>
+#include "Timer.h"
+
+using namespace std;
+
+class Foo {
+  private:
+    float cooldown; // Gift cooldown
+    bool ready; // If Foo is ready to give a gift
+    int type; // 0 is for food, 1 is for fuel
+    int amount; // Amount of gift
+    Timer timer; // Timer
+
+  public:
+    Foo() {
+      cooldown = 30; // 30 seconds
+      ready = true; // Foo is originally ready
+      type = rand() % 2; // Random gift
+      amount = rand() % 15 + 5; // Random amount
+      timer = Timer(); // Initialize timer
+    }
+};
