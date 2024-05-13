e3178f01d62a9e25174bbc82fb80e68783651140
Forest Young <111620017+Grocherio@users.noreply.github.com>
2024-05-13 12:37:00 -0600

    Add files via upload

---
+++ src/Compartment.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Compartment {
+  private: // Variables
+    float money; // Amount of money in the compartment
+    int type; // 0 is for Box, 1 is for Cabinet, 2 is for Safe
+    float time; // Time to search a compartment
+
+  public:
+    Compartment() {
+      type = rand() % 3; // Random type
+
+      money = rand() % 5 + 10;
+      time = 3;
+      for (int i = 0; i < type; i++) {
+        money *= 2;
+        time *= 1.5;
+      }
+    }
+};
